7e569d1cff52dfb38d607cd3e194f856fec3db86
Dmitry Belyaev <didima220@gmail.com>
2018-06-07 16:50:25 +0300

    Add ajax

---
+++ app/assets/javascripts/store.js.coffee
@@ -0,0 +1,11 @@
+setStore = ->
+  $('.navbar-select').on 'change', ->
+    store_id = $(this).val()
+    console.log(store_id)
+    $.ajax
+      type: 'GET'
+      url:  '/'
+      data: { selected_store: { store_id: store_id} }
+
+$(document).on 'ready ajaxSuccess', ->
+  setStore()
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,22 @@
 class StoreController < ApplicationController
+  before_action :set_store, only: :index
+
   def index
-    @store =  Store.first
     @stores = Store.all.map { |store| [store.name, store.id] }
+
+    respond_to do |format|
+      format.js   { render 'index.js.erb' }
+      format.html {}
+    end
+  end
+
+  private
+
+  def set_store
+    if params[:selected_store]
+      @store = Store.find(params[:selected_store][:store_id])
+    else
+      @store =  Store.first
+    end
   end
 end
